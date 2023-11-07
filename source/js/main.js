f47a017bb7e981ecd05d0eb0c9f2487ff1d86ea0
arina <arinasorokina825@gmail.com>
2023-11-08 00:48:10 +0300

    Adds the subs tabs

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,8 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {Form} from './modules/form-validate/form';
+import {initTabs} from './modules/tabs/init-tabs'
 import {initSwiperJury} from './modules/init-jury-swiper';
 import { initSwiperReview } from './modules/init-review-swiper';
+import {iosVhFix} from './utils/ios-vh-fix';
+import {Form} from './modules/form-validate/form';
 
 // ---------------------------------
 
@@ -18,12 +19,14 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    const form = new Form();
-    window.form = form;
-    form.init();
+    initTabs();
 
     initSwiperJury();
     initSwiperReview();
+
+    const form = new Form();
+    window.form = form;
+    form.init();
   });
 });
 
